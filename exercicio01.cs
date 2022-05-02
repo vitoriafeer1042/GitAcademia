1d984afa12fc7a3864f0238753fc83e54f5904ea
Vitória Fernanda <vitoriafeer1042@gmail.com>
2022-05-02 12:04:39 -0300

    Add files via upload

---
+++ exercicio01.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace exercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1, nota2, media;
+            Console.WriteLine("Informe as notas:");
+            nota1 = double.Parse(Console.ReadLine());
+            nota2 = double.Parse(Console.ReadLine());
+
+            media = (nota1 + nota2) / 2;
+            Console.WriteLine("A media das notas informadas eh:" + media);
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
