1bd020a986140d4fd9c6545fcbf429f6cb55374b
Vitória Fernanda <vitoriafeer1042@gmail.com>
2022-05-03 15:51:50 -0300

    Add files via upload

---
+++ exercicio004.cs
@@ -0,0 +1,4 @@
+﻿Build iniciado...
+1>------ Compilação iniciada: Projeto: questao004, Configuração: Debug Any CPU ------
+1>questao004 -> C:\Users\Vitória Fernanda\source\repos\questao004\questao004\bin\Debug\net5.0\questao004.dll
+========== Compilar: 1 com êxito, 0 com falha, 0 atualizados, 0 ignorados ==========
