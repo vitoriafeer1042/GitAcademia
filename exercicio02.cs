98428b21a39aaf5470da1b4dd5ead27a4d9ff65a
Vitória Fernanda <vitoriafeer1042@gmail.com>
2022-05-30 17:26:54 -0300

    Delete exercicio02.cs

--- exercicio02.cs
+++
@@ -1,4 +0,0 @@
-﻿Build iniciado...
-1>------ Compilação iniciada: Projeto: exercicio02, Configuração: Debug Any CPU ------
-1>exercicio02 -> C:\Users\Vitória Fernanda\source\repos\exercicio02\exercicio02\bin\Debug\net5.0\exercicio02.dll
-========== Compilar: 1 com êxito, 0 com falha, 0 atualizados, 0 ignorados ==========
